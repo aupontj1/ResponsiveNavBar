56e567f4fdf19263da1a0a233824d80efae4bcb0
aupontj1 <94812032+aupontj1@users.noreply.github.com>
2023-12-30 10:28:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let toggleBtn = document.querySelector('.toggle-btn');
+let toggleBtnIcon = document.querySelector('.toggle-btn i');
+let dropDownMenu = document.querySelector('.dropdown-menu');
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open');
+    let isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+}
